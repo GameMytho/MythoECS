63a02b3aa0f01774e5de9b8c4b9c8f7724ad6175
WLeGen <WLeGen@mytho.com>
2025-07-02 19:47:45 +0800

    add the imple and test of `basic_command_queue`

--- tests/src/ecs/commands.cc
+++ tests/src/ecs/commands.cc
@@ -16,58 +16,70 @@ struct Direction {
     float y;
 };
 
-TEST(CommandsTest, BasicTest) {
-    using entity = mytho::ecs::basic_entity<uint32_t, uint8_t>;
-    using registry = mytho::ecs::basic_registry<entity, uint8_t, uint8_t, 1024>;
-    using commands = mytho::ecs::basic_commands<registry>;
+using entity = mytho::ecs::basic_entity<uint32_t, uint8_t>;
+using registry = mytho::ecs::basic_registry<entity, uint8_t, uint8_t, 1024>;
+using commands = mytho::ecs::basic_commands<registry>;
 
+template<typename... Ts>
+using querier = mytho::ecs::basic_querier<registry, Ts...>;
+
+void startup(commands cmds, querier<entity, Position, Vectory, Direction> q) {
+    cmds.spawn(Position{1.0f, 2.0f}, Vectory{3.0f, 4.0f}, Direction{5.0f, 6.0f});
+
+    EXPECT_EQ(q.size(), 0);
+}
+
+void update(commands cmds, querier<entity, Position, Vectory, Direction> q) {
+    cmds.spawn(Position{10.0f, 20.0f}, Vectory{30.0f, 40.0f}, Direction{50.0f, 60.0f});
+
+    EXPECT_EQ(q.size(), 1);
+
+    for (auto [e, pos, vec, dir] : q) {
+        EXPECT_EQ(e.id(), 0);
+        EXPECT_EQ(e.version(), 0);
+        EXPECT_EQ(pos.x, 1.0f);
+        EXPECT_EQ(pos.y, 2.0f);
+        EXPECT_EQ(vec.x, 3.0f);
+        EXPECT_EQ(vec.y, 4.0f);
+        EXPECT_EQ(dir.x, 5.0f);
+        EXPECT_EQ(dir.y, 6.0f);
+    }
+}
+
+void shutdown(commands cmds, querier<entity, Position, Vectory, Direction> q) {
+    EXPECT_EQ(q.size(), 2);
+
+    for (auto [e, pos, vec, dir] : q) {
+        if (e.id() == 0) {
+            EXPECT_EQ(e.version(), 0);
+            EXPECT_EQ(pos.x, 1.0f);
+            EXPECT_EQ(pos.y, 2.0f);
+            EXPECT_EQ(vec.x, 3.0f);
+            EXPECT_EQ(vec.y, 4.0f);
+            EXPECT_EQ(dir.x, 5.0f);
+            EXPECT_EQ(dir.y, 6.0f);
+        } else if (e.id() == 1) {
+            EXPECT_EQ(e.version(), 0);
+            EXPECT_EQ(pos.x, 10.0f);
+            EXPECT_EQ(pos.y, 20.0f);
+            EXPECT_EQ(vec.x, 30.0f);
+            EXPECT_EQ(vec.y, 40.0f);
+            EXPECT_EQ(dir.x, 50.0f);
+            EXPECT_EQ(dir.y, 60.0f);
+        } else {
+            FAIL() << "This should not be reached";
+        }
+    }
+}
+
+TEST(CommandsTest, CommandsQueueTest) {
     registry reg;
-    commands cmds(reg);
 
-    auto e = cmds.spawn();
-    EXPECT_EQ(e.id(), 0);
-    EXPECT_EQ(e.version(), 0);
+    reg.add_startup_system<startup>();
+    reg.add_update_system<update>();
+    reg.add_shutdown_system<shutdown>();
 
-    cmds.insert(e, Position{0.1f, 1.1f});
-    EXPECT_EQ(reg.contain<Position>(e), true);
-    {
-        auto [pos] = reg.get<Position>(e);
-        EXPECT_EQ(pos.x, 0.1f);
-        EXPECT_EQ(pos.y, 1.1f);
-    }
-    
-    cmds.insert(e, Vectory{1.0f, 2.1f}, Direction{3.0f, 0.3f});
-    EXPECT_EQ((reg.contain<Position, Vectory, Direction>(e)), true);
-    {
-        auto [pos, vec, dir] = reg.get<Position, Vectory, Direction>(e);
-        EXPECT_EQ(pos.x, 0.1f);
-        EXPECT_EQ(pos.y, 1.1f);
-        EXPECT_EQ(vec.x, 1.0f);
-        EXPECT_EQ(vec.y, 2.1f);
-        EXPECT_EQ(dir.x, 3.0f);
-        EXPECT_EQ(dir.y, 0.3f);
-    }
-
-    cmds.remove<Vectory>(e);
-    EXPECT_EQ(reg.contain<Vectory>(e), false);
-    EXPECT_EQ((reg.contain<Position, Direction>(e)), true);
-    {
-        auto [pos, dir] = reg.get<Position, Direction>(e);
-        EXPECT_EQ(pos.x, 0.1f);
-        EXPECT_EQ(pos.y, 1.1f);
-        EXPECT_EQ(dir.x, 3.0f);
-        EXPECT_EQ(dir.y, 0.3f);
-    }
-
-    cmds.replace(e, Position{0.2f, 1.2f}, Direction{3.1f, 0.4f});
-    EXPECT_EQ((reg.contain<Position, Direction>(e)), true);
-    {
-        auto [pos, dir] = reg.get<Position, Direction>(e);
-        EXPECT_EQ(pos.x, 0.2f);
-        EXPECT_EQ(pos.y, 1.2f);
-        EXPECT_EQ(dir.x, 3.1f);
-        EXPECT_EQ(dir.y, 0.4f);
-    }
-
-    cmds.despawn(e);
+    reg.startup();
+    reg.update();
+    reg.shutdown();
 }
