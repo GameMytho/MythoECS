GameMytho/MythoECS

commit 42027c6327db81931be9dc7038c930ed3f87664a

opt: optimize the imple of entity/component operations

src/include/container/component_set.hpp
@@ -12,7 +12,7 @@ namespace mytho::container {
         mytho::utils::EntityType EntityT,
         typename ComponentT,
         typename AllocatorT,
-        size_t PageSize = 1024
+        size_t PageSize = 256
     >
     class basic_component_set : public basic_entity_set<EntityT, PageSize> {
     public:
@@ -36,23 +36,17 @@ namespace mytho::container {
         ~basic_component_set() { clear(); }
 
     public:
+        // must ensure entity exist
         template<typename... Ts>
         requires (sizeof...(Ts) > 0)
         void add(const entity_type& e, uint64_t tick, Ts&&... ts) {
-            ASSURE(!base_type::contain(e), "entity already exist.");
-
-            base_type::add(e);
-
-            auto idx = base_type::index(e);
+            auto idx = base_type::add(e);
             auto data_size = _cdata.size();
             if (idx >= data_size) {
-                _cdata.resize(idx + 1);
-                _ticks.resize(idx + 1);
+                allocator_type allocator{_cdata.get_allocator()};
 
-                for (int i = data_size; i < _cdata.size(); i++) {
-                    allocator_type allocator{_cdata.get_allocator()};
-                    _cdata[i] = alloc_traits::allocate(allocator, 1);
-                }
+                _cdata.resize(idx + 1, alloc_traits::allocate(allocator, 1));
+                _ticks.resize(idx + 1);
             }
 
             // use placement new, construct_at(c++20) need explicit constructor
@@ -64,29 +58,27 @@ namespace mytho::container {
             _ticks.set_changed_tick(idx, tick);
         }
 
+        // must ensure entity exist
         void remove(const entity_type& e) {
-            ASSURE(base_type::contain(e), "entity not exist.");
-
             auto idx = base_type::index(e);
+            auto last = base_type::size() - 1;
+
+            base_type::remove(e);
 
             // allocator_type allocator{_cdata.get_allocator()};
             // alloc_traits::destroy(allocator, _cdata[idx]);
             _cdata[idx]->~component_type();
 
-            if (idx != base_type::size() - 1) {
-                std::swap(_cdata[idx], _cdata[base_type::size() - 1]);
-                _ticks.set_added_tick(idx, _ticks.get_added_tick(base_type::size() - 1));
-                _ticks.set_changed_tick(idx, _ticks.get_changed_tick(base_type::size() - 1));
+            if (idx != last) {
+                std::swap(_cdata[idx], _cdata[last]);
+                _ticks.swap_ticks(idx, last);
             }
-
-            base_type::remove(e);
         }
 
+        // must ensure entity exist
         template<typename... Ts>
         requires (sizeof...(Ts) > 0)
         void replace(const entity_type& e, uint64_t tick, Ts&&... ts) {
-            ASSURE(base_type::contain(e), "entity not exist.");
-
             auto idx = base_type::index(e);
             _cdata[idx]->~component_type();
             new (_cdata[idx]) component_type{ std::forward<Ts>(ts)... };
@@ -94,42 +86,23 @@ namespace mytho::container {
             _ticks.set_changed_tick(idx, tick);
         }
 
-        void clear() {
-            for (unsigned int i = 0; i < base_type::size(); i++) {
-                _cdata[i]->~component_type();
-            }
-
-            for (unsigned int i = 0; i < _cdata.size(); i++) {
-                allocator_type allocator{_cdata.get_allocator()};
-                alloc_traits::deallocate(allocator, _cdata[i], 1);
-            }
-
-            _cdata.clear();
-            _ticks.clear();
-            base_type::clear();
-        }
-
+        // must ensure entity exist
         component_type& get(const entity_type& e) noexcept {
-            ASSURE(base_type::contain(e), "entity not exist.");
-
             return *_cdata[base_type::index(e)];
         }
 
+        // must ensure entity exist
         const component_type& get(const entity_type& e) const noexcept {
-            ASSURE(base_type::contain(e), "entity not exist.");
-
             return *_cdata[base_type::index(e)];
         }
 
+        // must ensure entity exist
         uint64_t& changed_tick(const entity_type& e) noexcept {
-            ASSURE(base_type::contain(e), "entity not exist.");
-
             return _ticks.get_changed_tick(base_type::index(e));
         }
 
+        // must ensure entity exist
         bool is_added(const entity_type& e, uint64_t tick) const noexcept {
-            ASSURE(base_type::contain(e), "entity not exist.");
-
             /*
              * if the component added system is same as the caller of this,
              * component added tick will be equal to the last run tick of the system,
@@ -138,9 +111,8 @@ namespace mytho::container {
             return _ticks.get_added_tick(base_type::index(e)) >= tick;
         }
 
+        // must ensure entity exist
         bool is_changed(const entity_type& e, uint64_t tick) const noexcept {
-            ASSURE(base_type::contain(e), "entity not exist.");
-
             /*
              * if the component changed system is same as the caller of this,
              * component changed tick will be equal to the last run tick of the system,
@@ -151,6 +123,21 @@ namespace mytho::container {
 
         bool contain(const entity_type& e) const noexcept {
             return base_type::contain(e);
+        }
+
+        void clear() {
+            for (unsigned int i = 0; i < base_type::size(); i++) {
+                _cdata[i]->~component_type();
+            }
+
+            allocator_type allocator{_cdata.get_allocator()};
+            for (unsigned int i = 0; i < _cdata.size(); i++) {
+                alloc_traits::deallocate(allocator, _cdata[i], 1);
+            }
+
+            _cdata.clear();
+            _ticks.clear();
+            base_type::clear();
         }
 
     public:

src/include/container/component_storage.hpp
@@ -37,50 +37,39 @@ namespace mytho::container {
         ~basic_component_storage() = default;
 
     public:
+        // must ensure the entity does not have any specific components
         template<mytho::utils::PureValueType... Ts>
         requires (sizeof...(Ts) > 0)
         void add(const entity_type& e, uint64_t tick, Ts&&... ts) {
-            ASSURE(not_contain<Ts...>(e), "the entity already has some components.");
-
             (assure<Ts>().add(e, tick, std::forward<Ts>(ts)), ...);
         }
 
+        // must ensure the entity has all specific components
         template<mytho::utils::PureValueType... Ts>
         void remove(const entity_type& e) {
             if constexpr (sizeof...(Ts) > 0) {
-                ASSURE(contain<Ts...>(e), "the entity is missing some components.");
                 (_remove_components<Ts>(e), ...);
             } else {
                 _remove_entity(e);
             }
         }
 
+        // must ensure the entity has all specific components
         template<mytho::utils::PureValueType... Ts>
         requires (sizeof...(Ts) > 0)
         std::tuple<const Ts&...> get(const entity_type& e) const noexcept {
-            ASSURE(contain<Ts...>(e), "the entity is missing some components.");
-
-            return _get<Ts...>(e);
+            return std::tuple<const Ts&...>(
+                static_cast<basic_component_set<entity_type, Ts, std::allocator<Ts>, PageSize>&>(
+                    *_pool[component_id_generator::template gen<Ts>()]
+                ).get(e)...
+            );
         }
 
+        // must ensure the entity has all specific components
         template<mytho::utils::PureValueType... Ts>
         requires (sizeof...(Ts) > 0)
         void replace(const entity_type& e, uint64_t tick, Ts&&... ts) {
-            ASSURE(contain<Ts...>(e), "the entity is missing some components.");
-
             (_replace(e, tick, std::forward<Ts>(ts)), ...);
-        }
-
-        template<mytho::utils::PureValueType... Ts>
-        requires (sizeof...(Ts) > 0)
-        bool contain(const entity_type& e) const noexcept {
-            return (_contain<Ts>(e) && ...);
-        }
-
-        template<mytho::utils::PureValueType... Ts>
-        requires (sizeof...(Ts) > 0)
-        bool not_contain(const entity_type& e) const noexcept {
-            return (_not_contain<Ts>(e) && ...);
         }
 
         template<mytho::utils::PureValueType... Ts>
@@ -104,6 +93,18 @@ namespace mytho::container {
             }
 
             return _entities[id];
+        }
+
+        template<mytho::utils::PureValueType... Ts>
+        requires (sizeof...(Ts) > 0)
+        bool contain(const entity_type& e) const noexcept {
+            return (_contain<Ts>(e) && ...);
+        }
+
+        template<mytho::utils::PureValueType... Ts>
+        requires (sizeof...(Ts) > 0)
+        bool not_contain(const entity_type& e) const noexcept {
+            return (!_contain<Ts>(e) && ...);
         }
 
         void clear() {
@@ -147,9 +148,10 @@ namespace mytho::container {
         }
 
         void _remove_entity(const entity_type& e) {
-            for (size_type i = 0; i < _pool.size(); i++) {
-                if (_pool[i] && _pool[i]->contain(e)) {
-                    _remove_funcs[i](_pool[i].get(), e);
+            for (size_type i = 0; i < _pool.size(); ++i) {
+                auto& p = _pool[i];
+                if (p && p->contain(e)) {
+                    _remove_funcs[i](p.get(), e);
 
                     // record the removed entity
                     if (i >= _entities.size()) {
@@ -157,19 +159,6 @@ namespace mytho::container {
                     }
                     _entities[i].push_back(e);
                 }
-            }
-        }
-
-        template<typename T, typename... Rs>
-        std::tuple<const T&, const Rs&...> _get(const entity_type& e) const noexcept {
-            using component_set_type = basic_component_set<entity_type, T, std::allocator<T>, PageSize>;
-
-            auto id = component_id_generator::template gen<T>();
-
-            if constexpr (sizeof...(Rs) > 0) {
-                return std::tuple_cat(std::tie(static_cast<component_set_type&>(*_pool[id]).get(e)), _get<Rs...>(e));
-            } else {
-                return std::tie(static_cast<component_set_type&>(*_pool[id]).get(e));
             }
         }
 
@@ -182,25 +171,18 @@ namespace mytho::container {
         }
 
         template<typename T>
-        bool _contain(const entity_type& e) const noexcept {
-            auto id = component_id_generator::template gen<T>();
-
-            return id < _pool.size() && _pool[id] &&  _pool[id]->contain(e);
-        }
-
-        template<typename T>
-        bool _not_contain(const entity_type& e) const noexcept {
-            auto id = component_id_generator::template gen<T>();
-
-            return id >= _pool.size() || !_pool[id] || !_pool[id]->contain(e);
-        }
-
-        template<typename T>
         bool _is_added(const entity_type& e, uint64_t tick) const noexcept {
             using component_set_type = basic_component_set<entity_type, T, std::allocator<T>, PageSize>;
 
             auto id = component_id_generator::template gen<T>();
-            return id < _pool.size() && static_cast<component_set_type&>(*_pool[id]).contain(e) && static_cast<component_set_type&>(*_pool[id]).is_added(e, tick);
+
+            if (id >= _pool.size()) {
+                return false;
+            }
+
+            auto& p = static_cast<component_set_type&>(*_pool[id]);
+
+            return p.contain(e) && p.is_added(e, tick);
         }
 
         template<typename T>
@@ -208,7 +190,27 @@ namespace mytho::container {
             using component_set_type = basic_component_set<entity_type, T, std::allocator<T>, PageSize>;
 
             auto id = component_id_generator::template gen<T>();
-            return id < _pool.size() && static_cast<component_set_type&>(*_pool[id]).contain(e) && static_cast<component_set_type&>(*_pool[id]).is_changed(e, tick);
+
+            if (id >= _pool.size()) {
+                return false;
+            }
+
+            auto& p = static_cast<component_set_type&>(*_pool[id]);
+
+            return p.contain(e) && p.is_changed(e, tick);
+        }
+
+        template<typename T>
+        bool _contain(const entity_type& e) const noexcept {
+            auto id = component_id_generator::template gen<T>();
+
+            if (id >= _pool.size()) {
+                return false;
+            }
+
+            auto& p = _pool[id];
+
+            return p &&  p->contain(e);
         }
 
     private:
@@ -220,9 +222,7 @@ namespace mytho::container {
             if (id >= _pool.size()) {
                 _pool.resize(id + 1);
                 _remove_funcs.resize(id + 1, nullptr);
-            }
 
-            if (_pool[id] == nullptr) {
                 _pool[id] = std::make_unique<component_set_type>();
                 _remove_funcs[id] = [](void* ptr, const entity_type& e){
                     static_cast<component_set_type*>(ptr)->remove(e);

src/include/container/entity_set.hpp
@@ -8,7 +8,7 @@
 #include "container/sparse_set.hpp"
 
 namespace mytho::container {
-    template<mytho::utils::EntityType EntityT, size_t PageSize = 1024>
+    template<mytho::utils::EntityType EntityT, size_t PageSize = 256>
     class basic_entity_set : public basic_sparse_set<typename EntityT::id_type, PageSize> {
     private:
         template<typename IdIteratorT, typename VersionIteratorT>
@@ -73,54 +73,58 @@ namespace mytho::container {
         virtual ~basic_entity_set() noexcept = default;
 
     public:
-        entity_type add(const entity_type& e) {
-            ASSURE(!contain(e), "invalid entity value(entity exist).");
-
-            base_type::add(e.id());
+        // must ensure entity not exist
+        auto add(const entity_type& e) {
             _versions.push_back(e.version());
-
-            return e;
+            return base_type::add(e.id());
         }
 
+        // must ensure entity exist
         void remove(const entity_type& e) noexcept {
-            ASSURE(contain(e), "invalid entity value(entity not exist).");
+            auto eid = e.id();
+            auto idx = base_type::index(eid);
 
-            if (base_type::index(e.id()) != _versions.size() - 1) {
-                _versions[base_type::index(e.id())] = _versions.back();
+            base_type::remove(eid);
+
+            if (idx != _versions.size() - 1) {
+                _versions[idx] = _versions.back();
             }
             _versions.pop_back();
-
-            base_type::remove(e.id());
         }
 
-        void swap(const entity_type& src, const entity_type& dst) noexcept {
-            ASSURE(contain(src) && contain(dst), "invalid entity value(entities not exist).");
+        // must ensure entity exist
+        std::pair<size_type, size_type> swap(const entity_type& src, const entity_type& dst) noexcept {
+            auto sid = src.id();
+            auto did = dst.id();
 
-            if (src.id() != dst.id()) {
-                base_type::swap(src.id(), dst.id());
-                std::swap(_versions[base_type::index(src.id())], _versions[base_type::index(dst.id())]);
-            }
+            auto ids = base_type::swap(sid, did);
+            std::swap(_versions[ids.first], _versions[ids.second]);
+
+            return ids;
+        }
+
+        // must ensure entity exist
+        size_type index(const entity_type& e) const noexcept {
+            return base_type::index(e.id());
         }
 
         bool contain(const entity_type& e) const noexcept {
-            return base_type::contain(e.id()) && base_type::index(e.id()) < _versions.size() && _versions[base_type::index(e.id())] == e.version();
+            if (!base_type::contain(e.id())) {
+                return false;
+            }
+
+            auto idx = base_type::index(e.id());
+
+            return idx < _versions.size() && _versions[idx] == e.version();
         }
 
-        size_type index(const entity_type& e) const noexcept {
-            ASSURE(contain(e), "invalid entity value(entity not exist).");
-
-            return base_type::index(e.id());
+        entity_type entity(size_type idx) const noexcept {
+            return entity_type(base_type::data(idx), _versions[idx]);
         }
 
         void clear() noexcept {
             base_type::clear();
             _versions.clear();
-        }
-
-        entity_type entity(size_type idx) const noexcept {
-            ASSURE(idx < _versions.size(), "entity index out of entity set!");
-
-            return entity_type(base_type::data(idx), _versions[idx]);
         }
 
     public:
@@ -136,9 +140,7 @@ namespace mytho::container {
 
     protected:
         void version_next(const entity_type& e) noexcept {
-            ASSURE(contain(e), "invalid entity value(entity not exist).");
-
-            _versions[base_type::index(e.id())]++;
+            ++_versions[base_type::index(e.id())];
         }
 
     private:

src/include/container/entity_storage.hpp
@@ -7,11 +7,12 @@
 #include "utils/idgen.hpp"
 
 namespace mytho::container {
+    // we must ensure entity exist before call `add`/`remove`/`has`/`not_has` functions
     template<
         mytho::utils::EntityType EntityT,
         typename ComponentIdGeneratorT,
         mytho::utils::UnsignedIntegralType ComponentIdT = size_t,
-        size_t PageSize = 1024
+        size_t PageSize = 256
     >
     class basic_entity_storage final : protected basic_entity_set<EntityT, PageSize> {
     public:
@@ -37,9 +38,11 @@ namespace mytho::container {
     public:
         template<mytho::utils::PureValueType... Ts>
         entity_type emplace() {
-            _length++;
+            ++_length;
 
-            if (_length <= base_type::size()) {
+            auto size = base_type::size();
+
+            if (_length <= size) {
                 if constexpr (sizeof...(Ts) > 0) {
                     (insert_components<Ts>(_map[_length - 1]), ...);
                 }
@@ -51,54 +54,55 @@ namespace mytho::container {
                     (insert_components<Ts>(_map.back()), ...);
                 }
 
-                return base_type::add(entity_type(base_type::size()));
+                entity_type e(size);
+
+                if (e.valid()) {
+                    base_type::add(e);
+                }
+
+                return e;
             }
         }
 
         void pop(const entity_type& e) noexcept {
-            ASSURE(contain(e), "invalid entity value(entity not exist).");
-
-            if (base_type::index(e) != (_length - 1)) {
-                _map[base_type::index(e)]->clear();
-                std::swap(_map[base_type::index(e)], _map[_length - 1]);
-                base_type::swap(e, base_type::entity(_length - 1));
-            }
-
+            auto idx = base_type::index(e);
             base_type::version_next(e);
 
-            _length--;
+            if (idx != (_length - 1)) {
+                base_type::swap(e, base_type::entity(_length - 1));
+                _map[idx]->clear();
+                std::swap(_map[idx], _map[_length - 1]);
+            }
+
+            --_length;
         }
 
         template<mytho::utils::PureValueType... Ts>
         requires (sizeof...(Ts) > 0)
         void add(const entity_type& e) {
-            ASSURE(contain(e), "invalid entity value(entity not exist).");
-
-            (insert_components<Ts>(_map[base_type::index(e)]), ...);
+            auto idx = base_type::index(e);
+            (insert_components<Ts>(_map[idx]), ...);
         }
 
         template<mytho::utils::PureValueType... Ts>
         requires (sizeof...(Ts) > 0)
         void remove(const entity_type& e) noexcept {
-            ASSURE(contain(e), "invalid entity value(entity not exist).");
-
-            (remove_components<Ts>(_map[base_type::index(e)]), ...);
+            auto idx = base_type::index(e);
+            (remove_components<Ts>(_map[idx]), ...);
         }
 
         template<mytho::utils::PureValueType... Ts>
         requires (sizeof...(Ts) > 0)
         bool has(const entity_type& e) const noexcept {
-            ASSURE(contain(e), "invalid entity value(entity not exist).");
-
-            return (_has<Ts>(e) && ...);
+            auto idx = base_type::index(e);
+            return (_has<Ts>(idx) && ...);
         }
 
         template<mytho::utils::PureValueType... Ts>
         requires (sizeof...(Ts) > 0)
         bool not_has(const entity_type& e) const noexcept {
-            ASSURE(contain(e), "invalid entity value(entity not exist).");
-
-            return (_not_has<Ts>(e) && ...);
+            auto idx = base_type::index(e);
+            return (!_has<Ts>(idx) && ...);
         }
 
         bool contain(const entity_type& e) const noexcept {
@@ -135,15 +139,9 @@ namespace mytho::container {
         }
 
         template<typename T>
-        bool _has(const entity_type& e) const noexcept {
+        bool _has(auto idx) const noexcept {
             auto id = component_id_generator::template gen<T>();
-            return _map[base_type::index(e)]->contain(id);
-        }
-
-        template<typename T>
-        bool _not_has(const entity_type& e) const noexcept {
-            auto id = component_id_generator::template gen<T>();
-            return !_map[base_type::index(e)]->contain(id);
+            return _map[idx]->contain(id);
         }
 
     private:

src/include/container/sparse_set.hpp
@@ -11,14 +11,14 @@
 #include "utils/concept.hpp"
 
 namespace mytho::container {
-    template<mytho::utils::UnsignedIntegralType T, size_t PageSize = 1024>
+    template<mytho::utils::UnsignedIntegralType T, size_t PageSize = 256>
     class basic_sparse_set {
     public:
         using data_type = T;
         using density_type = std::vector<data_type>;
         using size_type = typename density_type::size_type;
         using page_data_type = typename density_type::size_type;
-        using page_type = std::array<page_data_type, PageSize>;
+        struct alignas(64) page_type : public std::array<page_data_type, PageSize> {};
         using sparsity_type = std::vector<page_type>;
         using iterator = typename density_type::iterator;
         using const_iterator = typename density_type::const_iterator;
@@ -35,45 +35,52 @@ namespace mytho::container {
         ~basic_sparse_set() noexcept = default;
 
     public:
-        void add(data_type data) {
-            ASSURE(!contain(data), "invalid integral value(value exist).");
+        // must ensure data value not exist
+        size_type add(data_type data) {
+            auto size = _density.size();
 
+            expand(page(data))[offset(data)] = size;
             _density.push_back(data);
-            expand(page(data))[offset(data)] = _density.size() - 1;
+
+            return size;
         }
 
+        // must ensure data value exist
         void remove(data_type data) noexcept {
-            ASSURE(contain(data), "invalid integral value(value not exist).");
+            data_type last_data = _density.back();
+            page_data_type& data_ref = sparse_ref(data);
+            page_data_type pos = data_ref;
 
-            if (data != _density.back()) {
-                page_data_type pos = sparse_ref(data);
-                _density[pos] = _density.back();
-                sparse_ref(_density[pos]) = pos;
+            data_ref = data_null;
+            if (data != last_data) {
+                sparse_ref(last_data) = pos;
+                _density[pos] = last_data;
             }
-
-            sparse_ref(data) = data_null;
             _density.pop_back();
         }
 
-        void swap(data_type src, data_type dst) noexcept {
-            ASSURE(contain(src) && contain(dst), "invalid integral value(value not exist).");
+        // must ensure data value exist
+        std::pair<size_type, size_type> swap(data_type src, data_type dst) noexcept {
+            page_data_type& sref = sparse_ref(src);
+            page_data_type& dref = sparse_ref(dst);
 
-            if (src != dst) {
-                std::swap(sparse_ref(src), sparse_ref(dst));
-                std::swap(_density[sparse_ref(src)], _density[sparse_ref(dst)]);
-            }
+            std::swap(sref, dref);
+            std::swap(_density[sref], _density[dref]);
+
+            return {sref, dref};
+        }
+
+        // must ensure data value exist
+        size_type index(data_type data) const noexcept {
+            return _sparsity[page(data)][offset(data)];
         }
 
         bool contain(data_type data) const noexcept {
             ASSURE(data != data_null, "invalid integral value(value reach max).");
 
-            return page(data) < _sparsity.size() && _sparsity[page(data)][offset(data)] != data_null;
-        }
+            auto idx = page(data);
 
-        size_type index(data_type data) const noexcept {
-            ASSURE(contain(data), "invalid integral value(value not exist).");
-
-            return _sparsity[page(data)][offset(data)];
+            return idx < _sparsity.size() && _sparsity[idx][offset(data)] != data_null;
         }
 
         void clear() noexcept {
@@ -82,8 +89,6 @@ namespace mytho::container {
         }
 
         data_type data(size_type idx) const noexcept {
-            ASSURE(idx < _density.size(), "index out of sparse set!");
-
             return _density[idx];
         }
 
@@ -110,10 +115,10 @@ namespace mytho::container {
         size_type offset(data_type data) const noexcept { return data % PageSize; }
 
         page_type& expand(size_t idx) {
-            if (idx >= _sparsity.size()) {
-                size_t old_size = _sparsity.size();
+            auto old_size = _sparsity.size();
+            if (idx >= old_size) {
                 _sparsity.resize(idx + 1);
-                for (size_t i = old_size; i < _sparsity.size(); i++) {
+                for (size_t i = old_size; i < _sparsity.size(); ++i) {
                     std::fill(_sparsity[i].begin(), _sparsity[i].end(), data_null);
                 }
             }

src/include/container/tick_set.hpp
@@ -19,28 +19,33 @@ namespace mytho::container {
         ~basic_tick_set() noexcept = default;
 
     public:
-        uint64_t get_added_tick(size_type index) const noexcept {
+        uint64_t get_added_tick(size_type index) const {
             return _added_ticks[index];
         }
 
-        uint64_t& get_added_tick(size_type index) noexcept {
+        uint64_t& get_added_tick(size_type index) {
             return _added_ticks[index];
         }
 
-        void set_added_tick(size_type index, uint64_t tick) noexcept {
+        void set_added_tick(size_type index, uint64_t tick) {
             _added_ticks[index] = tick;
         }
 
-        uint64_t get_changed_tick(size_type index) const noexcept {
+        uint64_t get_changed_tick(size_type index) const {
             return _changed_ticks[index];
         }
 
-        uint64_t& get_changed_tick(size_type index) noexcept {
+        uint64_t& get_changed_tick(size_type index) {
             return _changed_ticks[index];
         }
 
-        void set_changed_tick(size_type index, uint64_t tick) noexcept {
+        void set_changed_tick(size_type index, uint64_t tick) {
             _changed_ticks[index] = tick;
+        }
+
+        void swap_ticks(size_type l, size_type r) {
+            std::swap(_added_ticks[l], _added_ticks[r]);
+            std::swap(_changed_ticks[l], _changed_ticks[r]);
         }
 
         void resize(size_type size, uint64_t value = 0) {

src/include/ecs/entity.hpp
@@ -11,12 +11,16 @@ namespace mytho::ecs {
         using version_type = EntityVersionT;
         using self_type = basic_entity<id_type, version_type>;
 
-        basic_entity(id_type id, version_type ver = 0) noexcept : _id(id), _ver(ver) { }
+        static constexpr id_type id_null = std::numeric_limits<id_type>::max();
+
+        basic_entity(id_type id = 0, version_type ver = 0) noexcept : _id(id), _ver(ver) { }
 
     public:
         id_type id() const noexcept { return _id; }
 
         version_type version() const noexcept { return _ver; }
+
+        bool valid() const noexcept { return _id != id_null; }
 
         friend bool operator==(const self_type& l, const self_type& r) noexcept { return l._id == r._id && l._ver == r._ver; }
 

commit 897c21728f4c862fcb7d16ecc66b13bc390d468c

fix: fix nullptr access and error init in component containers, introduced by commit 42027c6 and even earlier

src/include/container/component_set.hpp
@@ -45,8 +45,12 @@ namespace mytho::container {
             if (idx >= data_size) {
                 allocator_type allocator{_cdata.get_allocator()};
 
-                _cdata.resize(idx + 1, alloc_traits::allocate(allocator, 1));
+                _cdata.resize(idx + 1);
                 _ticks.resize(idx + 1);
+
+                for (auto i = data_size; i <= idx; i++) {
+                    _cdata[i] = alloc_traits::allocate(allocator, 1);
+                }
             }
 
             // use placement new, construct_at(c++20) need explicit constructor

src/include/container/component_storage.hpp
@@ -222,9 +222,11 @@ namespace mytho::container {
             if (id >= _pool.size()) {
                 _pool.resize(id + 1);
                 _remove_funcs.resize(id + 1, nullptr);
+            }
 
+            if (!_pool[id]) {
                 _pool[id] = std::make_unique<component_set_type>();
-                _remove_funcs[id] = [](void* ptr, const entity_type& e){
+                _remove_funcs[id] = [](void* ptr, const entity_type& e) {
                     static_cast<component_set_type*>(ptr)->remove(e);
                 };
             }