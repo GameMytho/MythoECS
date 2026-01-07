d9ccb031cfe03bc9b53132f75d08304374814244
WLeGen <WLeGen@mytho.com>
2026-01-07 17:50:01 +0800

    test: rewrite use cases

--- src/include/ecs/ecs.hpp
+++ src/include/ecs/ecs.hpp
@@ -1,82 +1,82 @@
 #pragma once
 #include "ecs/registry.hpp"
 
-namespace {
+namespace mecs {
     template<typename... Ts>
-    using mut = mytho::ecs::mut<Ts...>;
+    using Mut = mytho::ecs::mut<Ts...>;
 
     template<typename... Ts>
-    using with = mytho::ecs::with<Ts...>;
+    using With = mytho::ecs::with<Ts...>;
 
     template<typename... Ts>
-    using without = mytho::ecs::without<Ts...>;
+    using Without = mytho::ecs::without<Ts...>;
 
     template<typename... Ts>
-    using added = mytho::ecs::added<Ts...>;
+    using Added = mytho::ecs::added<Ts...>;
 
     template<typename... Ts>
-    using changed = mytho::ecs::changed<Ts...>;
+    using Changed = mytho::ecs::changed<Ts...>;
 
     template<typename T>
-    using data_wrapper = mytho::utils::internal::data_wrapper<T>;
+    using DataWrapper = mytho::utils::internal::data_wrapper<T>;
 
     template<typename... Ts>
-    using res = mytho::ecs::basic_resources<Ts...>;
+    using Res = mytho::ecs::basic_resources<Ts...>;
 
     template<typename... Ts>
-    using res_mut = mytho::ecs::basic_resources_mut<Ts...>;
+    using ResMut = mytho::ecs::basic_resources_mut<Ts...>;
 
-    using entity = mytho::ecs::basic_entity<uint32_t, uint8_t>;
-    using registry = mytho::ecs::basic_registry<entity, uint16_t, uint16_t, uint8_t, 256>;
-    using commands = mytho::ecs::basic_commands<registry>;
+    using Entity = mytho::ecs::basic_entity<uint32_t, uint8_t>;
+    using Registry = mytho::ecs::basic_registry<Entity, uint16_t, uint16_t, uint8_t, 256>;
+    using Commands = mytho::ecs::basic_commands<Registry>;
 
     template<typename... Ts>
-    using querier = mytho::ecs::basic_querier<registry, Ts...>;
+    using Querier = mytho::ecs::basic_querier<Registry, Ts...>;
 
     template<typename T>
-    using event_writer = mytho::ecs::basic_event_writer<T>;
+    using EventWriter = mytho::ecs::basic_event_writer<T>;
 
     template<typename T>
-    using event_mutator = mytho::ecs::basic_event_mutator<T>;
+    using EventMutator = mytho::ecs::basic_event_mutator<T>;
 
     template<typename T>
-    using event_reader = mytho::ecs::basic_event_reader<T>;
+    using EventReader = mytho::ecs::basic_event_reader<T>;
 
     template<typename T>
-    using removed_entities = mytho::ecs::basic_removed_entities<registry, T>;
+    using RemovedEntities = mytho::ecs::basic_removed_entities<Registry, T>;
 
     template<mytho::utils::FunctionType Func>
     auto system(Func&& func) {
-        return registry::system(std::forward<Func>(func));
+        return Registry::system(std::forward<Func>(func));
     }
 
     template<mytho::utils::PureComponentType T, mytho::utils::PureComponentType... Rs>
-    bool components_added(commands cmd) {
-        return cmd.template components_added<T, Rs...>();
+    bool ComponentsAdded(Commands cmds) {
+        return cmds.template components_added<T, Rs...>();
     }
 
     template<mytho::utils::PureComponentType T, mytho::utils::PureComponentType... Rs>
-    bool components_changed(commands cmd) {
-        return cmd.template components_changed<T, Rs...>();
+    bool ComponentsChanged(Commands cmds) {
+        return cmds.template components_changed<T, Rs...>();
     }
 
     template<mytho::utils::PureComponentType T, mytho::utils::PureComponentType... Rs>
-    bool components_removed(commands cmd) {
-        return cmd.template components_removed<T, Rs...>();
+    bool ComponentsRemoved(Commands cmds) {
+        return cmds.template components_removed<T, Rs...>();
     }
 
     template<mytho::utils::PureResourceType T, mytho::utils::PureResourceType... Rs>
-    bool resources_exist(commands cmd) {
-        return cmd.template resources_exist<T, Rs...>();
+    bool ResourcesExist(Commands cmds) {
+        return cmds.template resources_exist<T, Rs...>();
     }
 
     template<mytho::utils::PureResourceType T, mytho::utils::PureResourceType... Rs>
-    bool resources_added(commands cmd) {
-        return cmd.template resources_added<T, Rs...>();
+    bool ResourcesAdded(Commands cmds) {
+        return cmds.template resources_added<T, Rs...>();
     }
 
     template<mytho::utils::PureResourceType T, mytho::utils::PureResourceType... Rs>
-    bool resources_changed(commands cmd) {
-        return cmd.template resources_changed<T, Rs...>();
+    bool ResourcesChanged(Commands cmds) {
+        return cmds.template resources_changed<T, Rs...>();
     }
 }
--- tests/src/use_case/src/event.cc
+++ tests/src/use_case/src/event.cc
@@ -1,67 +1,55 @@
 #include <gtest/gtest.h>
-#include "ecs/ecs.hpp"
+#include <ecs/ecs.hpp>
+#include <random>
 
-struct Frame {
-    unsigned int value;
-};
+using namespace mecs;
 
-struct Damage {
-    float value;
-};
+namespace ebo {
+    struct Damage {
+        float value;
+    };
 
-void update1(event_writer<Damage> e) {
-    e.write(Damage { 10.0f });
-}
+    void damage_event_send(EventWriter<Damage> ew) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(1, 10);
 
-void update2(event_mutator<Damage> e, res<Frame> r) {
-    auto [frame] = r;
-
-    if (frame->value == 1) {
-        EXPECT_EQ(e.mutate().size(), 1);
-
-        for (auto& d : e.mutate()) {
-            d.value *= 0.5f;
+        auto count = dis(gen);
+        for (auto i = 0; i < count; ++i) {
+            ew.write(Damage { 1.0f });
         }
-    } else {
-        EXPECT_EQ(e.mutate().size(), 0);
+    }
+
+    void damage_event_adjust(EventMutator<Damage> em) {
+        auto& events = em.mutate();
+
+        for (auto& event : events) {
+            EXPECT_EQ(event.value, 1.0f);
+            event.value *= 0.5f;
+        }
+    }
+
+    void damage_event_receive(EventReader<Damage> er) {
+        const auto& events = er.read();
+
+        for (const auto& event : events) {
+            EXPECT_EQ(event.value, 0.5f);
+        }
     }
 }
 
-void update3(event_reader<Damage> e, res<Frame> r) {
-    auto [frame] = r;
+TEST(EventTest, BasicOperation) {
+    Registry reg;
 
-    if (frame->value == 1) {
-        EXPECT_EQ(e.read().size(), 1);
-
-        for (const auto& d : e.read()) {
-            EXPECT_EQ(d.value, 5.0f);
-        }
-    } else {
-        EXPECT_EQ(e.read().size(), 0);
-    }
-}
-
-void update_frame(res_mut<Frame> r) {
-    auto [frame] = r;
-
-    frame->value += 1u;
-}
-
-TEST(SystemTest, EventTest) {
-    registry reg;
-
-    reg.init_resource<Frame>(0u)
-       .init_event<Damage>()
-       .add_update_system(update1)
-       .add_update_system(system(update2).after(update1))
-       .add_update_system(system(update3).after(update2))
-       .add_update_system(system(update_frame).after(update3));
-
-    reg.ready();
+    reg.init_event<ebo::Damage>()
+       .add_update_system(ebo::damage_event_send)
+       .add_update_system(system(ebo::damage_event_adjust).after(ebo::damage_event_send))
+       .add_update_system(system(ebo::damage_event_receive).after(ebo::damage_event_adjust))
+       .ready();
 
     reg.startup();
 
-    reg.update();
-
-    reg.update();
+    for (auto i = 0; i < 100; ++i) {
+        reg.update();
+    }
 }
